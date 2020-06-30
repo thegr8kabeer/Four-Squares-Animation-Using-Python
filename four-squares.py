b4d79a94c36e0a048d4b05ed8e86a83f2acd5a57
Thegr8kabeer <gopu80371@gmail.com>
2020-06-30 19:52:16 +0500

    Update four-squares.py

--- four-squares.py
+++ four-squares.py
@@ -1,24 +1,34 @@
-# A simple python project made by Thegr8kabeer that uses the 'Turtle' module
-# Easy to understand syntax for the person who knows the basics of Turtle
+# A simple python project made by Thegr8kabeer that uses the 'Turtle' module.
+# Easy to understand syntax for the person who knows the basics of Turtle.
 # Feel free to edit my code and do some experiments!!!
 # Don't forget to follow me on instagram at https://instagram.com/thegr8kabeer/
 
 import turtle
 
 turtle_1 = turtle.Turtle()
-#You can  in the parentheses, if would change the speed of the turtle
+# You can chamge the speed of the turtle by writing in the paranthesis below.
 turtle_1.speed(5) 
 
+# Making the function that determines how and where the 'turtle' moves. 
 def square():
+    # Moves forward
     turtle_1.forward(100)
+    # Moves right
     turtle_1.right(90)
+    # Moves forward
     turtle_1.forward(100)
+    # Moves right
     turtle_1.right(90)
+    # Moves forward
     turtle_1.forward(100)
+    # Moves right
     turtle_1.right(90)
+    # Moves forward
     turtle_1.forward(100)
 
+# Making a for loop to determine how many joint squares do you want.
 for count in range(4):
     square()
 
+# To exit , just press any key
 input("Press any key to exit ...")
